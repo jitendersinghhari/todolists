5aaaf446dfd39f77a86597d86efe110921fbb2c8
Jitender Hari <jitender.hari@hotmail.com>
2015-07-01 12:14:08 -0700

    render_to_string

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_resolves_root_to_home_page_view(self):
@@ -13,9 +15,12 @@ class HomePageTest(TestCase):
         request=HttpRequest()
         #print(request) #scheme :http or https, POST:QueryDict:{},path:,method,resolver_match,''' get_host()'''
         response=home_page(request)#home_page view returns HttpResponse
-        print(response.content)
-        self.assertTrue(response.content.startswith(b'<html>'))# content:html send to user
-        self.assertIn(b'<title>Django</title>',response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))# content:html send to user
+        print (response.content)# returns byte object
+        expected_html=render_to_string('home.html')
+        #print(expected_html,' \n',response.content.decode())
+        self.assertEqual(response.content.decode(),expected_html)
+        '''self.assertTrue(response.content.strip( ).startswith(b'<html>'))# content:html send to user
+        self.assertIn(b'<title>To-do lists</title>',response.content)
+        self.assertTrue(response.content.strip().endswith(b'</html>'))# content:html send to user'''
 
 
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>Django</title></html>')
+    #return HttpResponse('<html><title>Django</title></html>')
+    return render(request,'home.html')#request used to genrate response,template to show, it returns Httpresponse
+    ''' django will search folder templates inside your app directory and it will 
+    build response based on content ot template'''
