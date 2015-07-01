a35b69074f5853b33c2f44853e1dcf72c1b2a74c
Jitender Hari <jitender.hari@hotmail.com>
2015-07-01 11:37:50 -0700

    Before templating plain html response

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,21 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
-
+from django.http import HttpRequest
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_resolves_root_to_home_page_view(self):
         found=resolve("/") #gives us which view does a Url maps, returns ResolverMatch() object
-        print(found.url_name)#home, it is mapping to name of urls.py pattern
+        #print(found.url_name)#home, it is mapping to name of urls.py pattern
         self.assertEqual(found.func,home_page)
+    
+    def test_home_page_returns_correct_html(self):
+        request=HttpRequest()
+        #print(request) #scheme :http or https, POST:QueryDict:{},path:,method,resolver_match,''' get_host()'''
+        response=home_page(request)#home_page view returns HttpResponse
+        print(response.content)
+        self.assertTrue(response.content.startswith(b'<html>'))# content:html send to user
+        self.assertIn(b'<title>Django</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))# content:html send to user
+
+
