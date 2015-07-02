e9953493c8c7a5a05fb4474c28a40362a6c640aa
Jitender Hari <jitender.hari@hotmail.com>
2015-07-02 11:57:20 -0700

    Our first redirect

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
-def home_page(request):
-    #return HttpResponse('<html><title>Django</title></html>')
-    return render(request,'home.html',{
-        'new_item_text':request.POST.get('item_text',''),
-    })#request used to genrate response,template to show, it returns Httpresponse
-    
-
-
-
-    ''' django will search folder templates inside your app directory and it will 
+#request used to genrate response,template to show, it returns Httpresponse
+''' django will search folder templates inside your app directory and it will 
     build response based on content ot template'''
+
+
+def home_page(request):
+    if request.method=="POST":
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    return render(request,'home.html')
+
+
+
+
+    
