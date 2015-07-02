2268e16f15b62c7974e70529f659181a573c3710
Jitender Hari <jitender.hari@hotmail.com>
2015-07-02 01:08:52 -0700

    before ORM

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table= self.browser.find_element_by_id('id_list_table')
+        rows=[table.find_element_by_tag_name('tr')]
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-do',self.browser.title)
@@ -24,10 +29,21 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers') # this will type automatiaclly into inputbox
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table= self.browser.find_element_by_id('id_list_table')
-        rows=[table.find_element_by_tag_name('tr')]
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item') 
+        inputbox.send_keys('Use peacock feathers to make a fly') 
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+
+
+
+
+        time.sleep(10)
+
+
         #self.fail('finsh the Test')
     
 
