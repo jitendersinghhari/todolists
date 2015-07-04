39bfc3a666976113a71eb2c741d521f15eaecda7
Jitender Hari <jitender.hari@hotmail.com>
2015-07-04 16:57:31 -0700

    chapter 6-first

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
 from selenium.webdriver.common.keys import Keys
-import unittest
-import time
+# import unittest
+# import time
 
 
 # Django receives the HTTp request, decides which view will handle it,
@@ -12,42 +12,57 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        # selenium will work after 3 seconds of page loading
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = [table.find_element_by_tag_name('tr')]
-        for row in rows:
-            print("content: %s" % row.text)
-        self.assertIn(row_text, [row.text for row in rows])
-
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         self.assertIn('To-do', self.browser.title)
-
         header_text = self.browser.find_element_by_tag_name(
             'h1').text  # get inside text of <h1>
         self.assertIn('To-do', header_text)
-
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # this will type automatiaclly into inputbox
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('play cricket')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table(
-            '2: play cricket')
+            '2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         self.fail('finsh the Test')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = [table.find_element_by_tag_name('tr')]
+        self.assertIn(row_text, [row.text for row in rows])
